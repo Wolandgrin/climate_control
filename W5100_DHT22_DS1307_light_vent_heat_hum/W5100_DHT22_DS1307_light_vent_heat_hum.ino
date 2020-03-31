b2e495fc213373e14e803effe0ec810040a25e63
Vladimir <vladimir.grin@gmail.com>
2020-03-31 17:45:29 +0200

    Improved without internet

--- W5100_DHT22_DS1307_light_vent_heat_hum/W5100_DHT22_DS1307_light_vent_heat_hum.ino
+++ W5100_DHT22_DS1307_light_vent_heat_hum/W5100_DHT22_DS1307_light_vent_heat_hum.ino
@@ -4,16 +4,16 @@
 #include <Ethernet.h>
 
 #define DHTTYPE DHT22
-#define relayHeatPin 8
-#define relayLightPin 4
-#define coolerPin 10
-#define humidifierPin 11
+#define relayHeatPin 2
+#define relayLightPin 3
+#define coolerPin 6
+#define humidifierPin 7
 
 RTC_DS1307 RTC;
 DHT dht(9, DHTTYPE);
 EthernetServer server(80);
 IPAddress ip(192, 168, 1, 100);
-String deviceId = "vFEEC96DE2707EDF";// "vE88ABDC967551EF"; //or vFEEC96DE2707EDF
+String deviceId = "vE88ABDC967551EF";// "vE88ABDC967551EF"; //or vFEEC96DE2707EDF
 const char* logServer = "api.pushingbox.com";
 byte mac[] = {0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};
 float temperature;
@@ -23,9 +23,9 @@ char curTime [9];
 unsigned long uptime=0; // variable storing system uptime
 int upshift=0; // Shift M(inutes) on the screen, according to number of current system Uptime digits
 int minCheck; // check for minutes to count whole system uptime properly
-int minTemp = 23;
+int minTemp = 25.5;
 int maxTemp = 27;
-int minHum = 60;
+int minHum = 70;
 int maxHum = 90;
 int humidState;
 int coolerState;
@@ -33,30 +33,49 @@ int checkTemp = minTemp; // Check for temprature treshhold
 int checkCool = maxTemp;
 static const unsigned long SEND_INTERVAL = 20000;
 static unsigned long lastRefreshTime = 0;
-String serverMsg = "";
+String serverMsg = "Nothing to send yet";
   
 void setup() {
   Serial.begin(9600);
   dht.begin();
   RTC.begin();
-//  RTC.adjust(DateTime(__DATE__, __TIME__));
+  RTC.adjust(DateTime(__DATE__, __TIME__));
   minCheck = RTC.now().minute();
   if (! RTC.isrunning())
   {
     RTC.adjust(DateTime(__DATE__, __TIME__));
   }
+  delay(2000);
+  //Ethernet.begin(mac);
   delay(1000);
-  Ethernet.begin(mac, ip);
-  delay(1000);
-  Serial.print("Ethernet IP: ");
-  Serial.println(Ethernet.localIP());
-  server.begin();
+  //Serial.print("Ethernet IP: ");
+  //Serial.println(Ethernet.localIP());
+  //server.begin();
   
   pinMode(coolerPin, OUTPUT);
   pinMode(relayLightPin, OUTPUT);
   pinMode(relayHeatPin, OUTPUT);
   pinMode(humidifierPin, OUTPUT);
-  Serial.println("|   Date   |   Time   |  Temp |  Hum  |Cooler| Heat|Light|Humid| Uptime |");
+  String beginMsg = "Started with the following ranges: \nTemperature from " + String(minTemp) + " to " + String(maxTemp) + "\nHumidity from " + String(minHum) + " to " + String(maxHum);
+  Serial.println(beginMsg);
+  Serial.println("Visual system test for:\n1. Cooler");
+  digitalWrite(coolerPin, HIGH);
+  delay(5000);
+  digitalWrite(coolerPin, LOW);
+  Serial.println("2. Humidifier");
+  digitalWrite(humidifierPin, HIGH);
+  delay(5000);
+  digitalWrite(humidifierPin, LOW);
+  Serial.println("3. Light");
+  digitalWrite(relayLightPin, HIGH);
+  delay(5000);
+  digitalWrite(relayLightPin, LOW);
+  Serial.println("4. Heat pad");
+  digitalWrite(relayHeatPin, HIGH);
+  delay(5000);
+  digitalWrite(relayHeatPin, LOW);
+  delay(2000);
+  Serial.print("|   Date   |   Time   |  Temp |  Hum  |Cooler| Heat|Light|Humid| Uptime |");
 }
 
 void loop() {
@@ -65,43 +84,42 @@ void loop() {
   sprintf(curTime, "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
   
   temperature = dht.readTemperature();
-  humidity = dht.readHumidity();
-  
+  humidity = dht.readHumidity() - 25;
+
   if(millis() - lastRefreshTime >= SEND_INTERVAL) {
     calculateDeltaTime(now); // getting system Uptime and setting shift for M(inutes)
     if(millis() - lastRefreshTime >= SEND_INTERVAL / 4) {
       handleParams(now);
       handleNotifs();
-      refreshServer();
+     // refreshServer();
     }
     lastRefreshTime += SEND_INTERVAL;
-  }
-  
+  } 
 }
 
 void handleNotifs() {
   String msg;
   if ((temperature > 0.0) && (humidity > 0.0)) {
-    if (temperature <= 22.0) {
+    if (temperature <= minTemp - 1.0) {
       msg = "Temperature is below minimum: ";
       msg += String(temperature);
-    } else if (temperature >= 27.0) {
+    } else if (temperature >= maxTemp + 1.0) {
       msg = "Temperature is above maximum: ";
       msg += String(temperature);
     }
-    if (msg.length() > 0 && (humidity >= 99.0 || humidity < 60.0)) { msg += "; "; }
-    
-    if (humidity >= 99.0){
+    if (msg.length() > 0 && (humidity == 100 || humidity < minHum - 5.0)) { msg += "; "; }
+
+    if (humidity == 100){
       msg += "Humidity is above maximum: ";
       msg += String(humidity);
-    } else if (humidity < 60.0) {
+    } else if (humidity < minHum - 5.0) {
       msg += "Humidity is below minimum: ";
       msg += String(humidity);
     }
   }  else {
     msg = "No data from MASTER";
   }
-  sendToPushingBox(msg);
+  //sendToPushingBox(msg);
   if (msg.length() > 0) {
     serverMsg = "Last notification sent at " + String(date) + " " + String(curTime) + ", message: " + msg;
   }
@@ -112,7 +130,7 @@ void handleParams(DateTime now){
     digitalWrite(humidifierPin, HIGH);
     humidState = 1;
     delay(8000);
-  } else if ((now.hour() == 06 and now.minute() == 58) or (now.hour() == 01 and now.minute() == 04)) { // turning on humidifier twice per day
+  } else if (now.minute() == 59) { // turning on humidifier every hour
     digitalWrite(humidifierPin, HIGH); 
     humidState = 1;
   } else {
@@ -121,13 +139,13 @@ void handleParams(DateTime now){
   }
 
   if (temperature > checkCool) {
-    if (digitalRead(coolerPin) == LOW){
+    if (coolerState == 0){
       digitalWrite(coolerPin,HIGH);
       coolerState = 1;
       checkCool -= 1;
       delay(2000);
     }
-  } else if ((now.hour() == 06 and now.minute() == 56) or (now.hour() == 01 and now.minute() == 04)) { // turning on ventilation twice per day
+  } else if ((now.hour() == 07 and now.minute() == 56) or (now.hour() == 16 and now.minute() == 57)) { // turning on ventilation twice per day
     digitalWrite(coolerPin,HIGH);
     coolerState = 1;  
   } else {
@@ -159,8 +177,9 @@ String handleSensors(int data) {
 
 void refreshServer() {
   EthernetClient client = server.available();
+  Serial.print("Here");
   if (client) {
-    Serial.print(" Web started. ");
+    Serial.print(" Web started ");
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
@@ -214,10 +233,7 @@ void refreshServer() {
     }
     delay(1);
     client.stop();
-    Serial.println("Web disconnected");
-  }
-  else {
-    Serial.println();
+    Serial.print("and disconnected");
   }
 }
 
@@ -225,16 +241,14 @@ void sendToPushingBox(String message) {
   if (message.length() > 0) {
     delay(3000);
     EthernetClient client2;
-    if (client2.connect("api.pushingbox.com", 80)) {
-      Serial.println ("Connected to pushingbox");
-            
+    Serial.print(" HERE2 ");
+    if (client2.connect("api.pushingbox.com", 80)) {            
       String postStr = "devid=";
       postStr += String(deviceId);
       postStr += "&message_param=";
       postStr += message;
       postStr += "\r\n\r\n";
 
-      Serial.println("Sending..");
       client2.print("POST /pushingbox HTTP/1.1\n");
       client2.print("Host: api.pushingbox.com\n");
       client2.print("Connection: close\n");
@@ -243,8 +257,8 @@ void sendToPushingBox(String message) {
       client2.print(postStr.length());
       client2.print("\n\n");
       client2.print(postStr);
-      Serial.print("POST sent: ");
-      Serial.println(postStr);
+      Serial.print(" POST sent with: ");
+      Serial.print(message);
     }
     client2.stop();
     delay(2000);
@@ -266,6 +280,7 @@ void calculateDeltaTime(DateTime now) {
   }
 
   minCheck = now.minute();
+  Serial.println();
   Serial.print("| ");
   Serial.print(date);
   Serial.print(" | ");
@@ -284,5 +299,6 @@ void calculateDeltaTime(DateTime now) {
   Serial.print(humidState);
   Serial.print("  |  ");
   Serial.print(uptime);
-  Serial.print("  |");
+  Serial.print("  |");  
+  Serial.print(serverMsg);
 }
